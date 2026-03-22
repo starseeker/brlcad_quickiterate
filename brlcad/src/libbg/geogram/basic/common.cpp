689f01cd2eafabc93552ecb46ddda8e765aa9853
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2026-03-22 00:14:26 -0400

    Remove version files

--- brlcad/src/libbg/geogram/basic/common.cpp
+++ brlcad/src/libbg/geogram/basic/common.cpp
@@ -47,7 +47,6 @@
 #include <geogram/numerics/multi_precision.h>
 #include <geogram/numerics/predicates.h>
 #include <geogram/delaunay/delaunay.h>
-#include <geogram/version.h>
 
 #include <sstream>
 #include <iomanip>
