5191fea5b0f9dace9c3237b30b65ee632f36da98
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2026-04-08 22:12:58 -0400

    Changes from Windows testing

--- brlcad/src/rt/view.c
+++ brlcad/src/rt/view.c
@@ -233,19 +233,24 @@ view_pixel(struct application *ap)
 	else if (g < 0) g = 0;
 	if (b > 255) b = 255;
 	else if (b < 0) b = 0;
-	if (r == ibackground[0] && g == ibackground[1] &&
-	    b == ibackground[2]) {
-	    r = inonbackground[0];
-	    g = inonbackground[1];
-	    b = inonbackground[2];
+	if (!benchmark) {
+	    if (r == ibackground[0] && g == ibackground[1] &&
+		b == ibackground[2]) {
+		r = inonbackground[0];
+		g = inonbackground[1];
+		b = inonbackground[2];
+	    }
 	}
 
-	/* Make sure it's never perfect black */
-	if (r==0 && g==0 && b==0 && benchmark==0)
+	/* Make sure it's never perfect black, even in benchmark mode.
+	 * A genuine hit that renders to (0,0,0) is indistinguishable from
+	 * a background miss, which breaks compositing.  The reference image
+	 * bench/ref/m35.pix was generated with this guard always active. */
+	if (r==0 && g==0 && b==0)
 	    b = 1;
     }
 
-    if (OPTICAL_DEBUG&OPTICAL_DEBUG_HITS) bu_log("rgb=%3d, %3d, %3d xy=%3d, %3d (%g, %g, %g)\n",
+    if (OPTICAL_DEBUG&OPTICAL_DEBUG_HITS)bu_log("rgb=%3d, %3d, %3d xy=%3d, %3d (%g, %g, %g)\n",
 						 r, g, b, ap->a_x, ap->a_y,
 						 V3ARGS(ap->a_color));
 
@@ -1606,6 +1611,10 @@ view_2init(struct application *ap, char *UNUSED(framename))
 	    break;
 #ifdef RTSRV
 	case BUFMODE_RTSRV:
+	    if (scanbuf) {
+		bu_free(scanbuf, "scanbuf [multi-line]");
+		scanbuf = NULL;
+	    }
 	    scanbuf = (unsigned char *)bu_malloc(srv_scanlen*pwidth + sizeof(long), "scanbuf [multi-line]");
 	    break;
 #endif
